6ba62c6ad35d3d2db8bd29693b130bba713ee53b
ashurbz <rbzashu1@gmail.com>
2020-08-10 02:38:17 +0530

    css

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-document.querySelector('#myFile').addEventListener("change",function(){
-    const reader = new FileReader();
+ document.getElementById("button").addEventListener("click",function(){
+   
+        const reader = new FileReader();
+    
+      let value=  document.getElementById("taskid").value;
+      let task = document.getElementById("task").value;
+      localStorage.setItem(value+"task",task );
+        reader.addEventListener("load", () =>{
+           localStorage.setItem(value, reader.result);
+        })
+           console.log(this);
+        reader.readAsDataURL(document.getElementById("myFile").files[0]);
+        reader.result=null;
+        document.getElementById("task").value="";
+        document.getElementById("taskid").value="";
+        document.getElementById("myFile").value=null;
 
-    reader.addEventListener("load", () =>{
-       localStorage.setItem("recent-image", reader.result);
-    })
 
-    reader.readAsDataURL(this.files[0]);
-});
+        alert("Image has been uploaded");
+
+        // setTimeout(function(){
+        //     value= "
+        // task= "";
+
+        // },1000)
+        
+    
+    
+
+ })
+
